a129c2d9576e1c2c829309de1ab71daa17d9f274
Carlos Ramirez <carlosjramirez11@gmail.com>
2020-02-18 20:39:27 -0800

    Context App is completed

--- src/components/Booklist.js
+++ src/components/Booklist.js
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
+import { AuthContext } from '../contexts/AuthContext';
 
 class Booklist extends Component {
   render() {
     return (
-      <div className='book-list'>
-        <ul>
-          <li>Harry Potter</li>
-          <li>Endor's Game</li>
-          <li>Brave New World</li>
-        </ul>
-      </div>
+      //Works for Class and Function, also lets more than one context be used
+      <AuthContext.Consumer>
+        {authContext => (
+          <ThemeContext.Consumer>
+            {themeContext => {
+              const { isAuthenticated, toggleAuth } = authContext;
+              const { isLightTheme, light, dark } = themeContext;
+              const theme = isLightTheme ? light : dark;
+              return (
+                <div
+                  className='book-list'
+                  style={{ color: theme.syntax, background: theme.bg }}
+                >
+                  <ul>
+                    <li style={{ background: theme.ui }}>Harry Potter</li>
+                    <li style={{ background: theme.ui }}>Endor's Game</li>
+                    <li style={{ background: theme.ui }}>Brave New World</li>
+                  </ul>
+                  <div onClick={toggleAuth}>
+                    {isAuthenticated ? 'Logged In' : 'Logged Out'}
+                  </div>
+                </div>
+              );
+            }}
+          </ThemeContext.Consumer>
+        )}
+      </AuthContext.Consumer>
     );
   }
 }
