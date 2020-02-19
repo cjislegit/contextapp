461141d42dd23eb8881dfbcf9a3f823c5311db33
Carlos Ramirez <carlosjramirez11@gmail.com>
2020-02-18 17:59:24 -0800

    First push

---
+++ src/components/Booklist.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Booklist extends Component {
+  render() {
+    return (
+      <div className='book-list'>
+        <ul>
+          <li>Harry Potter</li>
+          <li>Endor's Game</li>
+          <li>Brave New World</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Booklist;
